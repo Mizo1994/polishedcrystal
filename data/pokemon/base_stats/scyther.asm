c90126f598fdadab1152a7ded4a7c919854e7b12
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2024-10-08 01:40:22 +0200

    Update scyther.asm

--- data/pokemon/base_stats/scyther.asm
+++ data/pokemon/base_stats/scyther.asm
@@ -7,7 +7,7 @@
 	db NO_ITEM, NO_ITEM ; held items
 	dn GENDER_F50, HATCH_MEDIUM_SLOW ; gender ratio, step cycles to hatch
 
-	abilities_for SCYTHER, SWARM, TECHNICIAN, STEADFAST
+	abilities_for SCYTHER, SWARM, TECHNICIAN, SHARPNESS
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_BUG, EGG_BUG ; egg groups
 
