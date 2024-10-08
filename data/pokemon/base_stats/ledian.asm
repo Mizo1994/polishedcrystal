cc037d9e028e76cb67e04348b7d2714d40477f3d
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2024-10-08 02:19:54 +0200

    Update ledian.asm

--- data/pokemon/base_stats/ledian.asm
+++ data/pokemon/base_stats/ledian.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  55,  35,  50,  85,  55, 110 ; 390 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db  55,  95,  50,  85,  35, 110 ; 430 BST
+	db  55,  95,  50,  120,  35, 110 ; 465 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
