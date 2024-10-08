f929010bf8fe7ab9069ec5fdf0aba3edbf30beb7
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2024-10-08 02:19:12 +0200

    Update ledyba.asm

--- data/pokemon/base_stats/ledyba.asm
+++ data/pokemon/base_stats/ledyba.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  40,  20,  30,  55,  40,  80 ; 265 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db  40,  40,  30,  55,  20,  80 ; 265 BST
+	db  40,  55,  30,  55,  20,  80 ; 280 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
