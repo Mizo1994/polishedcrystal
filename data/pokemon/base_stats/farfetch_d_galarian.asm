06ca3fca3a7d053dba4d64723bb5b455eee6e11d
Mizo1994 <82087649+Mizo1994@users.noreply.github.com>
2024-10-08 02:05:54 +0200

    Update farfetch_d_galarian.asm

--- data/pokemon/base_stats/farfetch_d_galarian.asm
+++ data/pokemon/base_stats/farfetch_d_galarian.asm
@@ -7,7 +7,7 @@
 	db NO_ITEM, LEEK ; held items
 	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
-	abilities_for FARFETCH_D_GALARIAN, STEADFAST, SHARPNESS, SCRAPPY
+	abilities_for FARFETCH_D_GALARIAN, STEADFAST, SHEERFORCE, SCRAPPY
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_GROUND ; egg groups
 
